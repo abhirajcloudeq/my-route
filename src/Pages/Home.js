7fe58b8075a4203d1fd55f63b384cb253cb1401f
Abhiraj Chaudhary <133480538+abhirajcloudeq@users.noreply.github.com>
2023-08-04 13:44:39 +0530

    commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,9 @@
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>;
-        <a href="/Contact"><button>Contact</button></a>
-          <a href="/About">  <button>About</button></a>
+            <h1>Home</h1>
+          <a href="/Contact"><button>Contact</button></a>
+          <a href="/About"><button>About</button></a>
         
         
         </div>
